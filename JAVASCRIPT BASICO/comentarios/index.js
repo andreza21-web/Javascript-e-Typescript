082f96540262aeb3851678a5720e9e029e37aa1c
Andreza <andreza.21pessoa@gmail.com>
2021-12-05 18:49:13 -0300

    iniciando com javascript

---
+++ JAVASCRIPT BASICO/comentarios/index.js
@@ -0,0 +1,10 @@
+/**
+ *  Testes usando comentearios
+ */
+
+console.log("OLA MUNDO!!");
+console.log("Linha 1");
+console.log("Linha 2");
+console.log("Linha 3");
+//console.log("Linha 4");
+console.log("Linha 5");
